0bf3ab1746db589c6fc9e5ba0b76207e776826bd
vanor-cardozo <vanor.fc@gmail.com>
2023-03-13 17:55:10 -0300

    fix: Correção para uso do hook personalizado

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,7 +4,8 @@ import { priceFormatter } from '../../utils/formatter'
 import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
-  const summary = useSummary
+  const summary = useSummary()
+  console.log('************', summary)
 
   return (
     <SummaryContainer>
